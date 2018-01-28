9cbdd132386b5be3277fe0258860681326a9e8d5
junho <kanndu9@gmail.com>
2018-01-28 12:38:18 +0900

    StepAsideUnityChanのアセット
    0128　先生への確認用

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+
+    
+    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    private Rigidbody myRigidbody;
+    //前進するための力（追加）
+    private float forwardForce = 800.0f;
+    void Start()
+    {
+
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+    }
+}
